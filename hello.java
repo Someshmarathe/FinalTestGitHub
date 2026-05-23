9bcf113d2b3776c4e8b509d71629d43ff8cf84b3
someshmarathe <someshmarathe00@gamil.com>
2026-05-24 00:02:09 +0530

    added new feature

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ package Third;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello 1");
-          System.out.println("hello 1");
+        System.out.println("hello");
+        System.out.println("hello 123");
     }
 }
