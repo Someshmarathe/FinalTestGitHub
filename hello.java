d932501ef69bc30ac8575f93355337bb75bbd59d
someshmarathe <someshmarathe00@gamil.com>
2026-05-23 23:59:38 +0530

    add new data

---
+++ hello.java
@@ -0,0 +1,9 @@
+package Third;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello 1");
+          System.out.println("hello 1");
+    }
+}
